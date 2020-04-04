e67a2dfc9f47ec856efcba886f17212304b464d1
GOVINDDIXIT <govinddixit93@GMAIL.com>
2020-04-05 01:41:10 +0530

    Updates
    add axios
    create database
    connect database to frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course = Course.create(title: 'Hello World', description: 'Create a react app using ruby on rails')
+
+section = Section.create(title: 'Chapter 1', course: course )
+
+episodes = Episode.create([
+    { title: "1", description: "lorem", url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+    { title: "2", description: "lorem", url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+    { title: "3", description: "lorem", url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+    { title: "4", description: "lorem", url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section }
+])
